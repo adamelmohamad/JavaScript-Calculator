815625014111140253317f3d0218f163b914e256
adamelmohamad <37544116+adamelmohamad@users.noreply.github.com>
2018-06-30 16:15:59 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+function zerobtn(){
+	calculator.answer.value += '0'
+}
+
+function onebtn(){
+	calculator.answer.value += '1'
+}
+
+
+function twobtn(){
+	calculator.answer.value += '2'
+}
+
+
+function threebtn(){
+	calculator.answer.value += '3'
+}
+
+function fourbtn(){
+	calculator.answer.value += '4'
+}
+
+function fivebtn(){
+	calculator.answer.value += '5'
+}
+
+function sixbtn(){
+	calculator.answer.value += '6'
+}
+function sevenbtn(){
+	calculator.answer.value += '7'
+}
+
+function eightbtn(){
+	calculator.answer.value += '8'
+}
+
+function ninebtn(){
+	calculator.answer.value += '9'
+}
+
+function clearbtn(){
+calculator.answer.value = ''
+}
+
+function multiplybtn() {
+	calculator.answer.value += '*'
+}
+
+function dividebtn() {
+	calculator.answer.value += '/'
+}
+
+function plusbtn() {
+	calculator.answer.value += '+'
+}
+
+function subtractbtn() {
+	calculator.answer.value += '-'
+}
+
+function equalsbtn() {
+calculator.answer.value = eval(calculator.answer.value);
+}
+
+
+
+
+
+
+
